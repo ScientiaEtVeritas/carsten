ddcde188b0105d2004bccd58e91ecbb2c1cc6461
Thomas Jansen <t.jansen@sap.com>
2014-11-03 16:23:29 +0100

    add org to travis

--- .cf.sh
+++ .cf.sh
@@ -3,7 +3,7 @@
 # Download cf command line client
 wget -O - https://cli.run.pivotal.io/stable\?release\=linux64-binary\&source\=github | tar xvz -C .
 
-./cf login -a https://api.run.pivotal.io -u $CF_USER -p $CF_PASS
+./cf login -a https://api.run.pivotal.io -u $CF_USER -p $CF_PASS -o $CF_ORG
 
 echo $1
 
